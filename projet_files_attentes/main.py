af81ee6bb9c75e997e50ce6e8c73145529c740c1
LGF-pnj <lorna64fernandez@gmail.com>
2024-12-19 10:37:22 +0100

    Update main.py

--- projet_files_attentes/main.py
+++ projet_files_attentes/main.py
@@ -1,6 +1,13 @@
 from file import *
+from random import randint
+        
+def _creerClient(tick):
+    """cree un client à partir de l'horloge et le renvoie"""
+    c=Client(tick)
+    return c
 
 class Repartiteur:
+    #lorna
     def __init__(self,s):
         self.attenteTotale=0
         self.files=[]
@@ -10,37 +17,71 @@ class Repartiteur:
         raise NotImplemented()
     
     def sortie_client(self,num):
-        """retire un client de la file et ajoute son temps d'attente au total"""
+        """retire un client de la file et ajoute son temps d'attente au total. 
+        num est le numero du guichet."""
         raise NotImplemented()
     
 class RepartiteurUn(Repartiteur):
     def __init__(self, s):
         super().__init__(s)
+        f=File()
+        self.files.append(f)
+
+    def entree_client(self):
+        c = _creerClient()
+        self.files[0].ajouter(c)
+
+    def sortie_client(self, num):
+        c=self.files[0].retirer()
+        self.simul._totalAttente += (self.simul._tick - c.temps_arrivee)
+
 
 class RepartiteurAlea(Repartiteur):
     def __init__(self, s):
         super().__init__(s)
+        for _ in range(self.simul.nbGuichets):
+            f=File()
+            self.files.append(f)
+        
+    def entree_client(self):
+        c=_creerClient()
+        ifile = randint(0, len(self.files)-1)
+        self.files[ifile].ajouter(c)
+
+
+    def sortie_client(self, num):
+        #soit num le numero identifiant du guichet entre 1 et nbGuichets du simulateur inclus
+        c = self.files[num-1].retirer()#-1 car passage de numero à indice ds le tableau files
+        self.simul._totalAttente += (self.simul._tick - c.temps_arrivee)
+        #on peut faire en sorte de renvoyer le client retirer ( pour le guichet.) Souhaitable ?
+        
 
 class RepartiteurChoix(Repartiteur):
     def __init__(self, s):
         super().__init__(s)
+        for _ in range(self.simul.nbGuichets):
+            f=File()
+            self.files.append(f)
+
+    def entree_client(self):
+        pass
+
+    def sortie_client(self, num):
+        pass
     
 class Client:
+    #lorna
     def __init__(self,tick):
         self.temps_arrivee=tick
 
-class File:
-    def __init__(self):
-        pass
-    
 
 class Guichet:
-    def __init__(self):
-        pass
+    #ludivine
+    def __init__(self, s):
+        self.simul = s
+        self.traitement = None #jsp, a completer
 
     def tour(self):
-        pass
-
-class Simulateur:
-    def __init__(self):
+        #appelle sortie client du repartiteur
+        #fait : self.simul._clientsServis += 1 lorsqu'un client est traité (sort du guichet)
         pass
