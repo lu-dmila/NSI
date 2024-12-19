84a7a6e4bd0e1b04ab38044205b83b2cd4bf65d7
LGF-pnj <lorna64fernandez@gmail.com>
2024-12-19 10:43:01 +0100

    Update simulateur.py
    Ecrit fn config, ébauche de demarre, commentaires

--- projet_files_attentes/simulateur.py
+++ projet_files_attentes/simulateur.py
@@ -1,14 +1,37 @@
+
 class Simulateur:
+    repartiteurs = {"Un" : "RepartiteurUn", "Alea" : "RepartiteurAlea", "Choix" : "RepartiteurChoix"}
+
     def __init__(self):
         self._repartiteur=None
         self._tick=None
         self._clientsServis=None
         self.max=None
         self.nbTours=None
+        #attributs rajoutés
+        self.nbGuichets=None
+        self._totalAttente=None
 
     def config(self):
+        #lorna
         """configure le simulateur selon les inputs des utilisateurs"""
-        pass
+        print("Mode de répartition ?")
+        self._repartiteur=self.repartiteurs[input()]
+        print("Nombre de guichets ?")
+        self.nbGuichets=input()
+        print("temps de traitement maximal aux guichets ?")
+        self.max = input()
+        print("nombres de tours de la simulation ?")
+        self.nbTours = input()
 
-    def demare():
-        pass
+
+    def demarre(self):
+        """lance la simulation et retourne le nombre de clients servis et l'attente totale"""
+        #boucle qui decremente tick(tt le tps)(actions entre)
+        self._tick = 0
+        #...
+        return ("clients servis : "+str(self._clientsServis), "attente totale : "+str(self._totalAttente))
+
+
+
+
