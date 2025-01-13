bc1e3aba1b239ce5179f7e06ff6e5712b8e29dde
LGF-pnj <lorna64fernandez@gmail.com>
2025-01-13 16:25:32 +0100

    Update simulateur.py
    correction configv2

--- projet_files_attentes/simulateur.py
+++ projet_files_attentes/simulateur.py
@@ -1,12 +1,43 @@
-class Simulateur:
+from Main import *
 
-    def __init__(self):
+def mode_rep_valide(elem : str, self):
+    #lorna
+    if elem == "Un" : 
+        return RepartiteurUn(self)
+    if elem == "Aleatoire" : 
+        return RepartiteurAlea(self)
+    if elem == "Choix" : 
+        return RepartiteurChoix(self)
+    
+def reesayez(attribut, validation, erreur : str, valAttribut=0) :
+    #lorna
+        while attribut == valAttribut :
+            print(erreur)
+            attribut = validation(input())
+        return attribut
+
+def reesayezRep(attribut, validation, erreur : str, self, valAttribut=0) :
+    #lorna
+        while attribut == valAttribut :
+            print(erreur)
+            attribut = validation(input(), self)
+        return attribut
+
+def int_valide(elem : str):
+    #lorna
+    if elem.isdigit() and int(elem)>0 :
+        return int(elem)
+    return 0
+
+
+class Simulateur:
+    def __init__(self)::
         self._repartiteur=None
         self._tick=0
         self._clientsServis=0
         self.max=0
         self.nbTours=0
-        #attributs rajoutés
+         #attributs rajoutés
         self._totalAttente=0
         self.guichets=[]
 
@@ -16,6 +47,7 @@ class Simulateur:
         print("Configuration du simulateur")
         print("nombres de tours de la simulation ?")
         self.nbTours = int_valide(input())
+        #régler ca si on avait le temps : oral rip
         if self.nbTours == 0 :
             self.nbTours = reesayez(self.nbTours, int_valide, "Entrée invalide. Veuillez entrer un nombre strictement positif.")
         print("Nombre de guichets ?")
@@ -40,7 +72,6 @@ class Simulateur:
         else :
             self._repartiteur=RepartiteurUn(self)
 
-    
     def demarre(self):
         """lance la simulation et retourne le nombre de clients servis et l'attente totale"""
         #boucle qui decremente tick(tt le tps)(actions entre) lance un tour sur tous les guichets et ajoute un client a la file.
@@ -51,34 +82,3 @@ class Simulateur:
             self.repartiteur.entree_client()
             self._tick-=1
         return ("clients servis : "+str(self._clientsServis), "attente totale : "+str(self._totalAttente)
-    
-        
-
-def int_valide(elem : str):
-    #lorna
-    if elem.isdigit() and int(elem)>0 :
-        return int(elem)
-    return 0
-        
-def mode_rep_valide(elem : str, self):
-    #lorna
-    if elem == "Un" : 
-        return RepartiteurUn(self)
-    if elem == "Aleatoire" : 
-        return RepartiteurAlea(self)
-    if elem == "Choix" : 
-        return RepartiteurChoix(self)
-    
-def reesayez(attribut, validation, erreur : str, valAttribut=0) :
-    #lorna
-    while attribut == valAttribut :
-        print(erreur)
-        attribut = validation(input())
-    return attribut
-
-def reesayezRep(attribut, validation, erreur : str, self, valAttribut=0) :
-    #lorna
-    while attribut == valAttribut :
-        print(erreur)
-        attribut = validation(input(), self)
-    return attribut
