e1b1fcc806c7d39a63c441737940cdf51740d61f
lu-dmila <ludivine@gmail.com>
2025-03-10 17:25:39 +0100

    "utcd"

--- sql/exos.py
+++ sql/exos.py
@@ -1,3 +1,23 @@
 import csv
 import sqlite3 as sgbd
-cnx=sgbd.connect('/home/')
+
+class Usager:
+    def __init__(self,code_barre):
+        self.cnx = sgbd.connect('/home/mediatheque.db')
+        self.code_barre = self.code_barre
+
+    def __del__(self):
+        self.cnx.close()
+
+    def emprunter(self,isbn):
+        c = cnx.cursor()
+        c.execute( INSERT INTO emprunt VALUES('self.code_barre','self.isbn',) )
+
+
+    def rendre(self, isb):
+        
+    def emprunts(self):
+
+
+        
+    cnx=sgbd.connect('/home/')
